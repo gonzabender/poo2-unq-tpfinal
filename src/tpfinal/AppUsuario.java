9373be866d982dbdc3740bb1908767e60de28fce
Lucas Alvarez Garcia <Beta@PC>
2021-11-02 12:20:24 -0300

    AppUsuario con un cambio de modo interno, sin subclases

--- src/tpfinal/AppUsuario.java
+++ src/tpfinal/AppUsuario.java
@@ -1,16 +1,18 @@
 package tpfinal;
 
-public abstract class AppUsuario {
-
+public abstract class AppUsuario implements MovementSensor {
 
 	private SEM sem;
 	private String patente;
 	private int celular;
+	private boolean automatico; //false indica que la app no esta en automatico
+								//true indica que la app esta en automatico
 	
 	public AppUsuario(SEM sem, String patente, int celular) {
 		this.sem=sem;
 		this.celular=celular;
 		this.patente=patente;
+		this.automatico=false;
 	}
 
 	public SEM getSem() {
@@ -37,12 +39,42 @@ public abstract class AppUsuario {
 	public int consultaSaldo() {
 		return this.sem.consultarSaldo(this.celular);
 	}
-	
-	//Factory method
-	abstract AppUsuario cambiarModo();
-	
-	abstract void alertaInicioE();
 
-	abstract void alertaFinE();
+	//devuelve la hora de inicio de estacionamiento, la cantidad maxima de horas que es posible
+	//estacionar, y si no tiene suficiente saldo para estacionar devuelve el texto
+	//"Saldo insuficiente, Estacionamiento no permitido" (tambien podria hacerse con una clase y una excepcion)
+	public String iniciarEstacionamiento (){
+		return this.sem.iniciarEstacionamiento(this.celular,this.patente);
+	}
+
+	//devuelve la hora de iicio de estacionamiento, la hora de fin, la cantidad de horas estacionado
+	//y el costo del estacionamiento (en lugar de un string se podria usar una nueva clase)
+	public String finalizarEstacionamiento() {
+		return this.sem.finalizarEstacionamiento(this.celular);
+	}	
+	
+	public void cambiarModo() {
+		this.automatico= !this.automatico;
+		this.modoAutomatico();
+	}
+	
+	//A implementar con observer (puede que con un while, 
+	//siendo la condicion "automatico")
+	public void modoAutomatico() {
+		if (this.automatico){
+			//DO SOMETHING	
+		}else {
+			//STOP DOING IT
+		}
+	}
+	
+	//Observer
+	public void alertaInicioE() {
+		//DO SOMETHING
+	};
+	//Observer
+	public void alertaFinE() {
+		//DO SOMETHING
+	};
 	
 }
--- src/tpfinal/MovementSensor.java
+++ src/tpfinal/MovementSensor.java
@@ -1,13 +1,11 @@
 package tpfinal;
 
-import java.util.Observable;
-
 /*
 * Cada una determinada cantidad de segundos las apps que implementen esta interfaz
 * recibirán uno de estos dos mensajes por parte del teléfono celular en que se encuentran
 * instaladas. Un mismo mensaje puede (y suele) ser recibido de forma repetida.
 */
-public interface MovementSensor extends Observable{
+public interface MovementSensor{
 	//Indica que el desplazamiento se realiza a bordo de un vehículo.
 	public void driving();
 	//Indica que el desplazamiento se realiza caminando.
