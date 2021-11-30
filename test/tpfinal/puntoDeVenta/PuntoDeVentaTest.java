7b0938950ef7f0f0ca1c875a9615ca74b48d4b92
Ignacio Cervantes <ignacio.m.cervantes@gmail.com>
2021-11-30 11:46:45 -0300

    Mejorado el test punto de venta

--- test/tpfinal/puntoDeVenta/PuntoDeVentaTest.java
+++ test/tpfinal/puntoDeVenta/PuntoDeVentaTest.java
@@ -9,42 +9,56 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import tpfinal.app.usuario.Celular;
-import tpfinal.compras.Compra;
+import tpfinal.compras.estacionamientos.Estacionamiento;
 import tpfinal.sistema.SEM;
 
 public class PuntoDeVentaTest {
 
-	
+	/**
+	 * En este test, comprobamos que un punto de venta puede cargar un celular,
+	 * iniciar un estacionamiento, notificar al sistema que se realizo una compra y
+	 * tambien verificar la hora en la que finaliza el estacionamiento comprado.
+	 */
+
 	PuntoDeVenta pv;
 	SEM sem;
 	String patente;
 	Celular cel;
 	LocalTime algunFinal;
-	
+
 	@BeforeEach
 	public void setUp() {
-		cel=mock (Celular.class);
-		patente= "ASD123";
+		// Set up
+		cel = mock(Celular.class);
+		patente = "ASD123";
 		sem = new SEM();
-		pv= new PuntoDeVenta();
+		pv = new PuntoDeVenta();
 		pv.setSem(sem);
-		algunFinal= LocalTime.of(18, 0);
+		algunFinal = LocalTime.of(18, 0);
 	}
-	
+
 	@Test
 	public void testCargarCelularCargaElCelularYRegistraLaVenta() {
+		// Exercise
 		pv.cargarCelular(cel, 100);
-		
+
+		// Verify
 		verify(cel).cargarSaldo(100);
 		assertTrue(sem.getCompras().size() == 1);
 	}
 
 	@Test
 	public void testIniciarEstacionamientoEnUnaCompraPuntualLoInicia() {
-		pv.iniciarEstacionamiento(patente, algunFinal);;
-		
+		// Exercise
+		pv.iniciarEstacionamiento(patente, algunFinal);
+
+		// Verify 1
 		assertTrue(sem.getCompras().size() == 1);
 		assertTrue(sem.getEstacionamientosEnCurso().size() == 1);
+
+		// Verify 2
+		Estacionamiento e = sem.getEstacionamientosEnCurso().get(0);
+		assertTrue(e.getHorarioFin().equals(algunFinal));
 	}
 
 }
